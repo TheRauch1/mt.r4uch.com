2d997edbe34b7455c42f0a52f16c228b0ec14d29
Kaj Rauch <kajrauch@Kajs-MBP.local>
2019-06-01 21:46:14 +0200

    added title and measurements for 2E and 4E

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 //console.log(data)
 
-var table = document.getElementById("table")
+var tablehead = document.getElementById("tablehead")
+var tableheadrow = tablehead.insertRow(0)
+var tableheadname = tableheadrow.insertCell(0)
+var tableheadgrams = tableheadrow.insertCell(1)
+var tableheadtype = tableheadrow.insertCell(2)
+var tableheadtwoE = tableheadrow.insertCell(3)
+var tableheadfourE = tableheadrow.insertCell(4)
+
+tableheadname.innerHTML = "Name"
+tableheadgrams.innerHTML = "1E"
+tableheadtype.innerHTML = "Typ"
+tableheadtwoE.innerHTML = "2E"
+tableheadfourE.innerHTML = "4E"
+
+var tablebody = document.getElementById("tablebody")
 
 var txtValue
-var tr = table.getElementsByTagName("tr");
+var tr = tablebody.getElementsByTagName("tr");
 
 data.forEach(element => {
-    var row = table.insertRow(0)
+    var row = tablebody.insertRow(0)
     var name = row.insertCell(0)
     var grams = row.insertCell(1)
     var type = row.insertCell(2)
+    var twoE = row.insertCell(3)
+    var fourE = row.insertCell(4)
 
     name.innerHTML = element.name
     grams.innerHTML = element.grams + 'g'
     type.innerHTML = element.type
+    twoE.innerHTML = element.grams*2 + 'g'
+    fourE.innerHTML = element.grams*4 + 'g'
 });
 
 
